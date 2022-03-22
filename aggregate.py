ca55965079add18d0ffb2ad19f0ad40bd80d900c
Yashwanth <jyashwanthreddy373@gmail.com>
2022-03-22 19:43:21 +0530

    Seperate file for Firebase operations and aggregation boilerplate code

--- aggregate.py
+++ aggregate.py
@@ -1,3 +1,28 @@
-def aggregate(project_id: str): 
-    return "Error"
+import firebase
 
+
+
+# Aggregation function for TFlite models : Returns Ensemble model for the given TFlite models
+def aggregateTFlite(models):
+    pass
+
+
+
+#Aggregation function for SKlearn models : Returns Ensemble model for the given SKlearn models
+def aggregateSKlearn(models):
+    pass
+
+
+
+# Main aggregate function : Returns Final Ensemble Model
+def aggregate(project_id): 
+    models = firebase.downloadModels(project_id)
+    if models[0].type == "TFlite":
+        finalModel = aggregateTFlite(models)
+    elif models[0].type == "SKlearn":
+        finalModel = aggregateSKlearn(models)
+    
+    result = firebase.uploadModel(finalModel, project_id)
+
+    if(result.status == "success"):
+        return "success"
---
+++ firebase.py
@@ -0,0 +1,18 @@
+'''
+Firebase Credentials 
+'''
+
+
+
+
+
+
+
+# Download models from firebase : Returns list of models
+def downloadModels(project_id):
+    pass
+
+
+# Upload model to firebase : Returns response object with status property
+def uploadModel(model, project_id):
+    pass
