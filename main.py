16807199bd7b6d0f5eb38e9735d30562bd5d7c85
TarangGarlapally <taranggarlapally@gmail.com>
2022-05-17 22:47:58 +0530

    Merge branch 'dyn-test'

--- main.py
+++ main.py
@@ -3,13 +3,17 @@
 # http://127.0.0.1:8000/docs for API docs (swagger.ui)
 
 from urllib import response
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.responses import FileResponse
 from aggregate import aggregate
-from firebase import getGlobalModelFile, getGlobalModeldowloadURL, uploadModelToFirebase, downloadModels
 from fastapi.middleware.cors import CORSMiddleware
+from firebase import getGlobalModelFile, getGlobalModeldowloadURL, uploadModelToFirebase, downloadModels, getInputProcessorFile, getGlobalModelFileForResult
 from firebase_init import initializeFirebase
 from Prediction import Prediction
+import importlib
+import os
+import pickle
+import shutil
 
 initializeFirebase()
 app = FastAPI()
@@ -21,6 +25,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.get('/')
 def welcome():
@@ -54,7 +59,7 @@ def dowloadGlobalModelURLFromFirebase(project_id: str):
 @app.get('/getGlobalModelFile/{project_id}')
 async def getGlobalModelFileFromFirebase(project_id: str):
     await getGlobalModelFile(project_id)
-    return FileResponse('model-files/globalModel.pkl',media_type='application/octet-stream',filename=project_id)
+    return FileResponse('model-files/'+project_id+'.pkl',media_type='application/octet-stream',filename=project_id)
 
 @app.get('/specialCaseTimeSeries/{project_id}/predict/{periods}')
 def specialCaseTimeSeriesPredict(project_id: str, periods: int):
@@ -70,3 +75,26 @@ def specialCaseTimeSeriesPredict(project_id: str, periods: int):
 @app.post('/uploadModelToFirebase/{project_id}')
 async def uploadModelToFB(project_id : str , upload_file : UploadFile = File(...)):
     return await uploadModelToFirebase(project_id , upload_file)
+
+@app.post('/getModelResult/{project_id}')
+async def getModelResult(project_id: str, input: Request):
+    if os.path.exists("./__pycache__"):
+        shutil.rmtree("./__pycache__")
+        
+    # download input processor file
+    getInputProcessorFile(project_id)
+    # download global model
+    getGlobalModelFileForResult(project_id)
+    
+    model = None
+    # load model
+    with open("./model-files/"+project_id+".pkl", 'rb') as f:
+        model = pickle.load(f)
+
+    test = __import__(project_id)
+
+    result = test.processInputAndPredict(await input.json(), model)
+    
+    os.remove("./"+project_id+".py")
+    os.remove("./model-files/"+project_id+".pkl")
+    return result
